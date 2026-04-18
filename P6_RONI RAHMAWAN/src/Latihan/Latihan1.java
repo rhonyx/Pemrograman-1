c2e516059fec55e13bb984f2ba7120ff7514b0f1
RvS <ronirahmawan75@gmail.com>
2026-04-18 20:48:48 +0700

    Add files via upload

---
+++ P6_RONI RAHMAWAN/src/Latihan/Latihan1.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Roni Rahmawan
+ */
+public class Latihan1 {
+    public static void main(String args []){
+        Scanner input = new Scanner(System.in);
+        
+        int tipe_villa;
+        int hari;
+        int harga_permalam;
+        String nama_villa;
+        
+        System.out.print("Pilih tipe villa : (1.Standar,2.Premium,3.PVP): ");
+        tipe_villa = input.nextInt();
+        
+        System.out.print("Lama menginap : ");
+        hari = input.nextInt();
+        
+        if(tipe_villa == 1){
+               nama_villa = "Standar Vill";
+               harga_permalam = 300000;
+        }else if(tipe_villa == 2){
+            nama_villa = "Premium Vill";
+            harga_permalam = 500000;
+        }else if(tipe_villa == 3){
+            nama_villa = "PVP Vill";
+            harga_permalam = 1000000;
+        }else{
+            System.out.print("Tipe Villa Tidak Tersedia");
+            return;
+        }
+        
+        int total = hari * harga_permalam;
+        
+        System.out.println("------------------------------");
+        System.out.println("Villa yg dipesan    : "+ nama_villa);
+        System.out.println("Lama menginap       : "+ hari+" Hari");
+        System.out.println("Harga               : "+"Rp."+ total);
+        
+        input.close();
+    }
+    
+}
---
+++ P6_RONI RAHMAWAN/src/Latihan/Latihan3.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Roni Rahmawan
+ */
+public class Latihan3 {
+    public static void main(String args []){
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        
+        int tipe_villa;
+        int hari;
+        int harga_permalam;
+        String nama_villa;
+        int no_kamar;
+        
+        System.out.print("Pilih tipe villa : (1.Standar,2.Premium,3.PVP): ");
+        tipe_villa = input.nextInt();
+        
+        System.out.print("Lama menginap : ");
+        hari = input.nextInt();
+        
+        if(tipe_villa == 1){
+               nama_villa = "Standar Vill";
+               harga_permalam = 300000;
+               no_kamar = rand.nextInt(5)+1;
+        }else if(tipe_villa == 2){
+            nama_villa = "Premium Vill";
+            harga_permalam = 500000;
+            no_kamar = rand.nextInt(5)+6;
+        }else if(tipe_villa == 3){
+            nama_villa = "PVP Vill";
+            harga_permalam = 1000000;
+            no_kamar = rand.nextInt(5)+11;
+        }else{
+            System.out.print("Tipe Villa Tidak Tersedia");
+            return;
+        }
+        
+        int diskon = 0;
+        
+        if(hari>10){
+            diskon = 20;
+        }else if(hari>14){
+            diskon = 30;
+        }
+        
+        int total = hari * harga_permalam;
+        int total_diskon = total * diskon / 100;
+        int total_harga = total - total_diskon;
+        
+        System.out.println("------------------------------");
+        System.out.println("Villa yg dipesan    : "+ nama_villa);
+        System.out.println("Nomer Kamar         : "+no_kamar);
+        System.out.println("Lama menginap       : "+ hari+" Hari");
+        System.out.println("Harga               : "+"Rp."+ total);
+        System.out.println("Diskon              : "+"Rp."+total_diskon);
+        System.out.println("Harga Total         : "+"Rp."+ total_harga);
+
+        
+        input.close();
+    }
+}
